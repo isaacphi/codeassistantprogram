8aed5f1893113c622605736c4aae17a6e836ba3a
Phil <phil@pheedloop.com>
2024-07-20 20:58:04 +0900

    Working with anthropic

---
+++ cmd/root.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/henomis/lingoose/llm/antropic"
+	"github.com/henomis/lingoose/thread"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "cap",
+	Short: "The cap code assistant program",
+	Long: `Use cap to interact with LLMs using branching threads
+WIP`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("args", args)
+
+		antropicllm := antropic.New().WithModel("claude-3-opus-20240229").WithStream(
+			func(response string) {
+				if response != antropic.EOS {
+					fmt.Print(response)
+				} else {
+					fmt.Println()
+				}
+			},
+		)
+
+		t := thread.New().AddMessage(
+			thread.NewUserMessage().AddContent(
+				thread.NewTextContent("How are you?"),
+			),
+		)
+
+		err := antropicllm.Generate(context.Background(), t)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(t)
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.codeassistantprogram.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.22.5
 require github.com/spf13/cobra v1.8.1
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/henomis/lingoose v0.2.0 // indirect
+	github.com/henomis/restclientgo v1.2.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
